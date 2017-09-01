4128e9f9ca61707c5d87d663189e109a50f3579b
Marc Siegel <marc@usainnov.com>
2017-09-01 16:24:58 -0400

    WIP

--- flow-core/examples/gutenberg_bigrams.rb
+++ flow-core/examples/gutenberg_bigrams.rb
@@ -2,31 +2,69 @@
 
 # Can we demo a reactive streams publisher of lines from `tar jtvf <file>`?
 
+require "singleton"
 require "childprocess"
 
+class NilSubscription
+  include Singleton
+
+  def request(_); end
+
+  def cancel; end
+end
+
+class PublisherStateError < StandardError; end
+
 class ChildProcessPublisher
-  attr_reader :process
 
   def initialize(process)
     @process = process
-    verify_initial_state
+    @subscriber = nil
+    check_process_not_started
+    setup_finalizer
     setup_pipe
-    #setup_finalizer
   end
 
-  def subscribe
+  def subscribe(subscriber)
+    if @subscriber.nil?
+      @subscriber = subscriber
+      @subscriber.on_subscribe(Subscription.new(self))
+    else
+      subscriber.on_subscribe(NilSubscription.instance)
+      subscriber.on_error(PublisherStateError.new())
+    end
+
+  end
 
   private
 
-  def setup_pipe
-    @read_io, w = IO.pipe
-    process.io.stdout = w
-    process.io.stderr = $stderr
+  def check_process_not_started
+    raise ArgumentError, "Invalid ChildProcess: #{@process.inspect}" if
+      @process.nil? || @process.send(:started?)
   end
 
-  def verify_initial_state
-    raise ArgumentError, "Invalid ChildProcess: #{process.inspect}" if
-      process.nil? || process.send(:started?)
+  def check_no_subscriber
+    raise
+  end
+
+  def setup_finalizer
+    ObjectSpace.define_finalizer(self, Finalizer.new(@process))
+  end
+
+  def setup_pipe
+    @read_io, @process.io.stdout = IO.pipe
+    @process.io.stderr = $stderr
+  end
+
+  # @see ObjectSpace#define_finalizer
+  class Finalizer
+    def initialize(process)
+      @process = process
+    end
+
+    def call(*)
+      @process&.stop(1)
+    end
   end
 end
 
