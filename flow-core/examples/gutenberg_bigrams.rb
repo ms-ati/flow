372d393ced6c610a3a0a0722a86b3de132052269
Marc Siegel <marc@usainnov.com>
2017-09-11 13:05:32 -0400

    WIP

--- flow-core/examples/gutenberg_bigrams.rb
+++ flow-core/examples/gutenberg_bigrams.rb
@@ -6,20 +6,167 @@
 # 1. Spikes of reactive streams level
 ##
 
-require "singleton"
 require "childprocess"
+require "values"
 
-class NilSubscriptionClass
-  include Singleton
+module ReactiveStreams
+  module API
+    class Publisher
+      def subscribe(subscriber)
+        raise NotImplementedError
+      end
+    end
 
-  def request(_); end
+    class Subscriber
+      def on_subscribe(subscription)
+        raise NotImplementedError
+      end
 
-  def cancel; end
+      def on_next(element)
+        raise NotImplementedError
+      end
+
+      def on_error(error)
+        raise NotImplementedError
+      end
+
+      def on_complete
+        raise NotImplementedError
+      end
+    end
+
+    class Subscription
+      def request(n)
+        raise NotImplementedError
+      end
+
+      def cancel
+        raise NotImplementedError
+      end
+    end
+  end
+
+  module Tools
+    class NilSubscriberClass < ReactiveStreams::API::Subscriber
+      private_class_method :new
+      def on_subscribe(_); end
+      def on_next(_); end
+      def on_error(_); end
+      def on_complete; end
+    end
+
+    NIL_SUBSCRIBER = NilSubscriberClass.send(:new)
+
+    class NilSubscriptionClass < ReactiveStreams::API::Subscription
+      private_class_method :new
+      def request(_); end
+      def cancel(_); end
+    end
+
+    NIL_SUBSCRIPTION = NilSubscriptionClass.send(:new)
+
+    class PumpingPublisher < ReactiveStreams::API::Publisher
+      DEFAULT_SCHEDULE = Thread.method(:new)
+      DEFAULT_BATCH_SIZE = 1024
+
+      def initialize(
+        get_next:,
+        only_one: true,
+        schedule: DEFAULT_SCHEDULE,
+        batch_size: DEFAULT_BATCH_SIZE
+      )
+        @get_next = get_next
+        @only_one = only_one
+        @schedule = schedule
+        @batch_size = batch_size
+        @subscriptions = []
+      end
+
+      def subscribe(subscriber)
+        ensure_different(subscriber)
+        ensure_only_one(subscriber) if @only_one
+        subscription = PumpingSubscription.
+                       new(subscriber, @get_next, @schedule, @batch_size)
+        subscription.start # constructor finishes before concurrency starts
+        @subscriptions << subscription
+      end
+
+      private
+
+      # @see https://github.com/reactive-streams/reactive-streams-jvm/tree/v1.0.1#1.10
+      def ensure_different(subscriber)
+        unless @subscriptions.find { |s| s.subscriber == subscriber }.nil?
+          m = "Publisher#subscribe called > 1 time with: #{subscriber.inspect}"
+          subscriber.on_error(ReactiveStreamsError.new(m))
+        end
+      end
+
+      def ensure_only_one(subscriber)
+        unless @subscriptions.empty?
+          m = "#{self.class.to_s}#subscribe called > 1 time with only_one: true"
+          subscriber.on_error(ReactiveStreamsError.new(m))
+        end
+      end
+
+      class PumpingSubscription < ReactiveStreams::API::Subscription
+        def initialize(subscriber, get_next, schedule, batch_size)
+          @subscriber = subscriber
+          @get_next = get_next
+          @schedule = schedule
+          @batch_size = batch_size
+
+          @cancelled = false
+          @demand = 0
+
+          @pending_signals = Queue.new
+        end
+
+        def start
+          signal(:subscribe)
+        end
+
+        def request(n)
+          signal(:request, n)
+        end
+
+        def cancel
+          signal(:cancel)
+        end
+
+        private
+
+        def signal(s, args = nil)
+          @pending_signals << (args ? { s => args } : s)
+          try_to_schedule
+        end
+
+        def try_to_schedule
+          @running ||= @schedule.call(&self.method(:run))
+        end
+
+        def run
+          s = @pending_signals.pop(true) rescue nil
+
+          case s
+          when :subscribe
+            do_subscribe
+          when Hash
+            do_request(s[:request])
+          when :cancel
+            # TODO
+          end
+
+          @running = nil
+        end
+      end
+    end
+
+    class ReactiveStreamsError < StandardError; end
+  end
 end
 
-NilSubscription = NilSubscriptionClass.instance
 
-class PublisherStateError < StandardError; end
+
 
 class ChildProcessPublisher
 
@@ -86,8 +233,6 @@ end
 # 3. Spikes of flow dsl for building and running the above
 ##
 
-require "values"
-
 module Flow
   class Pusher
     def initialize(get_next:, for_each: [])
@@ -96,7 +241,7 @@ module Flow
     end
 
     def each(callable = nil, &block)
-      f = callable || block || (raise ArgumentError)
+      f = callable || block || (raise ArgumentError, "Needs block or callable")
       self.class.new(
         get_next: @get_next,
         for_each: @for_each + [f]
@@ -104,10 +249,11 @@ module Flow
     end
 
     def go!
-      loop do
-        n = @get_next.call
-        @for_each.each { |f| f.call(n) }
-      end
+
+      # loop do
+      #   n = @get_next.call
+      #   @for_each.each { |f| f.call(n) }
+      # end
     end
   end
 
@@ -122,7 +268,7 @@ module Flow
 
 end
 
-# First, just push the file we want to parse
+# First, just push the file(s) we want to parse
 files = Flow.pusher(["tmp/rdf-files.tar.bz2"])
 files = files.each { |l| puts l } # lazy
 puts "\nWait for it... (lazy)\n"
@@ -130,7 +276,16 @@ sleep(0.5)
 puts "---\nFiles:"
 files.go!
 
-# skip downloading the file for now, add that later
+# Nouns of the flow dsl:
+#   - Puller: Pulls elements synchronously
+#   - Pusher: Pushes elements asynchronously
+#   - Target: Receives from Pusher, can be pulled from
+
+# Second, create one process per file to parse
+# processes = Flow.
+#             pusher(["tmp/rdf-files.tar.bz2"])
+
+# Third, represent output of process(es) as flow
 # lines = Flow.
 #         pusher(["tmp/rdf-files.tar.bz2"]).
 #         via_process { |f| ["tar", "jtvf", f] }
