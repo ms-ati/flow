3f31a530873daf495c22939febe8688b5a55e78f
Marc Siegel <marc@usainnov.com>
2017-10-03 16:54:51 -0400

    Rubocop fixes

--- flow-base/lib/flow/base/signaller.rb
+++ flow-base/lib/flow/base/signaller.rb
@@ -22,32 +22,29 @@ module Flow
       #   pending signals. Required if *any* signals are provided by name,
       #   rather than as a map from symbol to proc callback.
       #
-      # @param signals_per_run [Integer]
+      # @param num_per_run [Integer]
       #   How many signals the main loop will process in one run before
       #   rescheduling itself to run again.
       #
       # @param runner [Runner]
       #   How we'll run the signaller's main loop (usually asynchronously).
       #
-      # rubocop:disable Metrics/ParameterLists
       def initialize(
         signals:,
-        target:          nil,
-        runner:          RUN_IN_NEW_THREAD,
-        signals_per_run: 2,
-        on_error:        Logger.new($stderr).method(:error),
-        callback_prefix: "do_"
+        target:,
+        runner:      RUN_IN_NEW_THREAD,
+        num_per_run: 2,
+        on_error:    Logger.new($stderr).method(:error)
       )
-        # rubocop:enable Metrics/ParameterLists
         # Error handling is solely via `on_error`
         @on_error = on_error
 
         # Signals and their mapping to callbacks (usually methods on the target)
-        @callbacks_by_signal = setup_callbacks(signals, target, callback_prefix)
+        @callbacks_by_signal = setup_callbacks(signals, target)
 
         # Configure how we'll run the signaller's main loop
         @runner = runner
-        @signals_per_run = signals_per_run
+        @num_per_run = num_per_run
 
         # Only one signaler main loop can run at a time
         @is_running = AtomicBoolean.new
@@ -83,36 +80,38 @@ module Flow
 
       private
 
-      # An instance of an anonymous class compares equal only to itself
+      # By default, signal names are prefixed to find callback methods on target
+      CALLBACK_PREFIX = "do_"
+
+      # An instance of an anonymous class, to compare equal only to itself
       NO_PENDING_SIGNALS = Class.new.new.freeze
 
-      # Run the main signal processing loop
+      # The main signal processing loop
       def main_loop
         # Establishes a happens-before relationship with end of previous run
-        return unless @running.value
+        return unless @is_running.value
 
-        num_to_process = @signals_per_run
+        num_left = @num_per_run
 
         loop do
           next_signal, args = next_pending_signal
           call_signal_callback(next_signal, args)
 
-          num_to_process -= 1
-          break if num_to_process.zero? || next_signal == NO_PENDING_SIGNALS
+          break if (num_left -= 1).zero? || NO_PENDING_SIGNALS == next_signal
         end
       rescue StandardError => error
         terminate_due_to(error)
       ensure
         # Establishes a happens-before relationship with beginning of next run
-        @running.value = false
+        @is_running.value = false
 
         # If we still have signals to process, schedule ourselves to run again
         try_to_run_main_loop unless @pending_signals.empty?
       end
 
-      # @return [Symbol, NO_PENDING_SIGNALS]
-      #   Return the next pending signal (and optional arguments) in the queue,
-      #   or a special value indicating there are no pending signals.
+      # @return [NO_PENDING_SIGNALS, Symbol, Array(Symbol, Array)]
+      #   Return the next pending signal (optionally with arguments) in the
+      #   queue, or a special value indicating there are no pending signals.
       #
       def next_pending_signal
         @pending_signals.pop(true)
@@ -121,12 +120,12 @@ module Flow
       end
 
       def call_signal_callback(next_signal, args)
-        return if @is_cancelled || next_signal == NO_PENDING_SIGNALS
+        return if @is_cancelled || NO_PENDING_SIGNALS == next_signal
 
         signal_callback = @callbacks_by_signal[next_signal] || begin
           msg = "#{self.class.name} received an unrecognized signal: "\
                 "#{next_signal.inspect}"
-          raise Flow::Error.new(msg)
+          raise Flow::Error, msg
         end
 
         signal_callback.call(*args)
@@ -134,10 +133,7 @@ module Flow
 
       def try_to_run_main_loop
         # Only schedule to run if not already running
-        if @is_running.make_true
-          @runner.call(&@main_loop)
-        end
-
+        @runner.call(&@main_loop) if @is_running.make_true
       rescue StandardError => error
         # If we can't schedule to run, we need to fail gracefully
         terminate_due_to(error)
@@ -150,21 +146,19 @@ module Flow
 
       # @param signals [Array<Symbol, Hash{Symbol => Proc}>]
       # @param target [Object]
-      # @param callback_prefix [String]
       # @return [Hash{Symbol => Proc}]
-      def setup_callbacks(signals, target, callback_prefix)
+      def setup_callbacks(signals, target)
         signals.inject({}) do |hsh, sym_or_hsh|
-          hsh.merge(callback_mapping_for(sym_or_hsh, target, callback_prefix))
+          hsh.merge(callback_mapping_for(sym_or_hsh, target))
         end.freeze
       end
 
       # @param sym_or_hsh [Symbol, Hash{Symbol => Proc}]
       # @param target [Object]
-      # @param callback_prefix [String]
       # @return [Hash{Symbol => Proc}]
-      def callback_mapping_for(sym_or_hsh, target, callback_prefix)
+      def callback_mapping_for(sym_or_hsh, target)
         if sym_or_hsh.is_a? Symbol
-          callback = target.method("#{callback_prefix}#{sym_or_hsh}".to_sym)
+          callback = target.method("#{CALLBACK_PREFIX}#{sym_or_hsh}".to_sym)
           { sym_or_hsh => callback }
         else
           sym_or_hsh
--- flow-base/lib/flow/errors.rb
+++ flow-base/lib/flow/errors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Flow
   Error = Class.new(StandardError)
 end
