751e717aec93a006669a663b051a4d8e03362e88
Marc Siegel <marc@usainnov.com>
2017-10-06 23:05:38 -0400

    Add first specs of Signaller

--- Gemfile
+++ Gemfile
@@ -8,6 +8,5 @@ group :development, :test do
   # gem "concurrent-ruby"
   gem "rake", "~> 12.0"
   gem "rspec", "~> 3.6"
-  gem "rspec-wait", "0.0.9"
   gem "rubocop", "~> 0.49.1", require: false
 end
--- flow-base/spec/flow/base/signaller_spec.rb
+++ flow-base/spec/flow/base/signaller_spec.rb
@@ -3,7 +3,9 @@ require "flow/base/signaller"
 RSpec.describe Flow::Base::Signaller do
   context "when used in a class via composition" do
     subject(:composed) do
-      composing_class.new
+      c = composing_class.new
+      c.mutex.lock if should_lock
+      c
     end
 
     before :each do
@@ -12,12 +14,14 @@ RSpec.describe Flow::Base::Signaller do
       composed.bar(42)
     end
 
+    let(:should_lock) { false }
+
     let(:composing_class) do
       Class.new do
         attr_reader :callbacks_received, :errors_seen, :signaller, :mutex
 
         def initialize
-          @callbacks_received = {}
+          @callbacks_received = Hash.new(0)
 
           @errors_seen = []
 
@@ -43,15 +47,47 @@ RSpec.describe Flow::Base::Signaller do
         end
 
         def do_bar(n)
-          @callbacks_received[:bar] += n
+          @mutex.synchronize { @callbacks_received[:bar] += n }
         end
       end
     end
 
-    context "when no locks" do
-      it "does all callbacks" do
-        wait_for { composed.callbacks_received[:foo] }.to eq(2)
-        wait_for { composed.callbacks_received[:bar] }.to eq(42)
+    def wait_for(secs = 3)
+      raise ArgumentError, "wait_for requires a block" unless block_given?
+      wait_until = Time.now + secs
+
+      loop do
+        begin
+          return yield
+        rescue RSpec::Expectations::ExpectationNotMetError
+          raise if Time.now >= wait_until
+          sleep 0.01
+          next
+        end
+      end
+    end
+
+    context "when no lock" do
+      it "processes all signals asynchronously" do
+        wait_for { expect(composed.callbacks_received[:foo]).to eq(2) }
+        wait_for { expect(composed.callbacks_received[:bar]).to eq(42) }
+      end
+    end
+
+    context "when a mutex is locked and unlocked" do
+      let(:should_lock) { true }
+
+      it "signal processing stops and starts" do
+        wait_for { expect(composed.callbacks_received[:foo]).to eq(2) }
+
+        3.times do
+          sleep 0.1
+          expect(composed.callbacks_received[:bar]).to eq(0)
+        end
+
+        composed.mutex.unlock
+
+        wait_for { expect(composed.callbacks_received[:bar]).to eq(42) }
       end
     end
   end
